8a3b769782f08af5e2a68fd69627ed0be921ed81
AKR0SS <AKR0SS@github.com>
2020-02-04 15:22:38 -0500

    GUI SettingsManager

--- main/Dawn.java
+++ main/Dawn.java
@@ -2,16 +2,44 @@ package mc_Dawn.main;
 
 import org.lwjgl.opengl.Display;
 
+import de.Hero.settings.SettingsManager;
 import mc_Dawn.main.event.EventManager;
+import mc_Dawn.main.event.EventTarget;
+import mc_Dawn.main.event.events.EventKey;
+import mc_Dawn.main.modules.ModuleManager;
 
 public class Dawn {
 	public String Title = "1.12.2 Dawn Client",
 				  Version = "0.1",
 				  Author = "AKROSS";
 	
+	public static Dawn instance = new Dawn();
+	
+	public SettingsManager settingsManager;
 	public EventManager eventManager;
+	public ModuleManager moduleManager;
 	
 	public void startClient() {
+		settingsManager = new SettingsManager();
 		eventManager = new EventManager();
+		moduleManager = new ModuleManager();
+		
+		
+		System.out.println("[" + Title + "] Starting . . . . . . ." + 
+				   "\n" + "v" + Version + 
+				   "\n" + "Created by " + Author);
+
+		Display.setTitle(Title + " v" + Version);
+		
+		eventManager.register(this);
+	}
+	
+	public void stopClient() {
+		eventManager.unregister(this);
+	}
+	
+	@EventTarget
+	public void onKey(EventKey event) {
+		moduleManager.getModules().stream().filter(module -> module.getKey() == event.getKey()).forEach(module -> module.toggle());
 	}
 }
