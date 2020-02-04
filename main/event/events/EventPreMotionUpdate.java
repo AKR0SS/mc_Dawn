f6bda5a5ec3643a30c2fc068d15d710df40af4c8
AKR0SS <AKR0SS@github.com>
2020-02-04 15:23:03 -0500

    (X,Y,Z) Coords

--- main/event/events/EventPreMotionUpdate.java
+++ main/event/events/EventPreMotionUpdate.java
@@ -6,10 +6,16 @@ public class EventPreMotionUpdate extends Event {
 	private float yaw, pitch;
 	private boolean ground;
 	
-	public EventPreMotionUpdate(float yaw, float pitch, boolean ground) {
+	private double x, y, z;
+	
+	public EventPreMotionUpdate(float yaw, float pitch, boolean ground, double x, double y, double z) {
 		this.yaw = yaw;
 		this.pitch = pitch;
 		this.ground = ground;
+		
+		this.x = x;
+		this.y = y;
+		this.z = z;
 	}
 	
 	public float getYaw() {
