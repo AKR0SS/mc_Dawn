f327d958bb1514a5180eccb6420180abefbab4c0
AKR0SS <AKR0SS@github.com>
2020-02-04 15:23:30 -0500

    @Override Disable

--- main/modules/movement/Sprint.java
+++ main/modules/movement/Sprint.java
@@ -2,6 +2,8 @@ package mc_Dawn.main.modules.movement;
 
 import org.lwjgl.input.Keyboard;
 
+import mc_Dawn.main.event.EventTarget;
+import mc_Dawn.main.event.events.EventUpdate;
 import mc_Dawn.main.modules.Category;
 import mc_Dawn.main.modules.Module;
 
@@ -10,9 +12,16 @@ public class Sprint extends Module {
 		super("Sprint", Keyboard.KEY_Z, Category.MOVEMENT);
 	}
 	
-	/*@EventTarget
+	@EventTarget
 	public void onUpdate(EventUpdate event) {
 		if(!mc.player.isCollidedHorizontally && mc.player.moveForward > 0)
 			mc.player.setSprinting(true);
-	}*/
+	}
+	
+	@Override
+	public void onDisable() {
+		super.onDisable();
+		
+		mc.player.setSprinting(false);
+	}
 }
