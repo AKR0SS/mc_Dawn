26df61abaf7f8574b4e18f42354dbab29020fb65
AKR0SS <AKR0SS@github.com>
2020-02-07 13:22:47 -0500

    Slider that does nothing :)

--- mc_Dawn/main/modules/movement/Step.java
+++ mc_Dawn/main/modules/movement/Step.java
@@ -1,15 +1,28 @@
 package mc_Dawn.main.modules.movement;
 
+import java.util.ArrayList;
+
 import org.lwjgl.input.Keyboard;
 
+import de.Hero.settings.Setting;
+import mc_Dawn.main.Dawn;
 import mc_Dawn.main.event.EventTarget;
 import mc_Dawn.main.event.events.EventUpdate;
 import mc_Dawn.main.modules.Category;
 import mc_Dawn.main.modules.Module;
 
 public class Step extends Module {
+	String mode = Dawn.instance.settingsManager.getSettingByName("Step Height").getValString();
+	
 	public Step() {
 		super("Step", Keyboard.KEY_NONE, Category.MOVEMENT);
+	}
+	
+	@Override
+	public void setup() {
+		ArrayList<String> options = new ArrayList<>();
+		
+		Dawn.instance.settingsManager.rSetting(new Setting("Step Height", this, 1, .5, 4, false));
 	}
 	
 	@EventTarget
