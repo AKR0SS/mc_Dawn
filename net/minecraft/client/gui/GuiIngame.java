1826bd4b9342ec87d079d88681cf47a42c4c7100
AKR0SS <AKR0SS@github.com>
2020-02-08 12:14:05 -0500

    private to public Minecraft mc

--- net/minecraft/client/gui/GuiIngame.java
+++ net/minecraft/client/gui/GuiIngame.java
@@ -74,7 +74,7 @@ public class GuiIngame extends Gui
     private static final ResourceLocation WIDGETS_TEX_PATH = new ResourceLocation("textures/gui/widgets.png");
     private static final ResourceLocation PUMPKIN_BLUR_TEX_PATH = new ResourceLocation("textures/misc/pumpkinblur.png");
     private final Random rand = new Random();
-    private final Minecraft mc;
+    public final Minecraft mc;
     private final RenderItem itemRenderer;
 
     /** ChatGUI instance that retains all previous chat data */
