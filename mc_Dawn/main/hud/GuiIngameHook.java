830de7ed7d000af112ed03a68e15833b3be31619
AKR0SS <AKR0SS@github.com>
2020-02-08 20:32:07 -0500

    Initial Commit

---
+++ mc_Dawn/main/hud/GuiIngameHook.java
@@ -0,0 +1,35 @@
+package mc_Dawn.main.hud;
+
+import mc_Dawn.main.Dawn;
+import mc_Dawn.main.modules.Module;
+import mc_Dawn.main.modules.ModuleManager;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.gui.GuiIngame;
+import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.client.renderer.GlStateManager;
+
+public class GuiIngameHook extends GuiIngame {
+	public GuiIngameHook(Minecraft mcIn) {
+		super(mcIn);
+	}
+	
+	@Override
+	public void renderGameOverlay(float partialTicks) {
+		mc.fontRendererObj.drawString("Dawn Client", 900, 4, 0xFFCCFF);
+	
+		super.renderGameOverlay(partialTicks);
+		
+		ScaledResolution scaledresolution = new ScaledResolution(this.mc);
+		FontRenderer frontrednerer = this.getFontRenderer();
+		GlStateManager.enableBlend();
+		
+		int count = 0;
+		
+		for(Module m: Dawn.instance.moduleManager.getModules()) {
+			if(m.isToggled()) {
+				mc.fontRendererObj.drawString(m.getName(), 4, 6 + (count*10), 0xFFCCFF);
+			}
+		}
+	}
+}
